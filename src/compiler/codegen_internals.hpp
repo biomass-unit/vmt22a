55a5dd23f6d1dbee65ba613f36246d0242588aa6
Collum <Collum@protonmail.ch>
2022-03-26 15:46:15 +0200

    Removed the old codegen scope

--- src/compiler/codegen_internals.hpp
+++ src/compiler/codegen_internals.hpp
@@ -10,30 +10,13 @@
 
 namespace compiler {
 
-    struct Scope {
-        struct Binding {
-            bu::Wrapper<ast::Type> type;
-            vm::Local_offset_type  frame_offset;
-        };
-
-        bu::Flatmap<lexer::Identifier, Binding> bindings;
-        vm::Virtual_machine*                    machine;
-        Scope*                                  parent;
-
-        auto find(lexer::Identifier) noexcept -> Binding*;
-
-        auto make_child() & -> Scope;
-    };
-
     struct Codegen_context {
         vm::Virtual_machine* machine;
         ir::Program          program;
-        Scope                scope;
 
         Codegen_context(vm::Virtual_machine& machine, ir::Program&& program) noexcept
             : machine { &machine }
-            , program { std::move(program) }
-            , scope   { .machine = &machine, .parent = nullptr } {}
+            , program { std::move(program) } {}
     };
 
 }
